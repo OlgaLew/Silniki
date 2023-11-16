3964efa21b82e28c5eed4f179b7747f69eaeb7d3
OlgaLew <olgalewandowska17l@interia.pl>
2023-11-16 10:42:21 +0100

    Baza_gry

---
+++ Gra_platformowa_Włóczka/Assets/Scripts/KeyAndDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyAndDoor : MonoBehaviour
+{
+    public GameObject Door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Triggered by Player");         
+            Destroy(Door.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Gra_platformowa_Włóczka/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Obstacles : MonoBehaviour
+{
+    public bool restartGame = false;
+    public Transform checkPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Triggered by Player");
+            //Destroy(Door.gameObject);
+            if (restartGame){
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            else
+            {
+                other.transform.position = checkPoint.position;
+            }
+            
+
+        }
+    }
+}
